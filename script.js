30860341c36a11f6d5c08bf50e5f97c2eb3bd8cb
AhmadGDev <Ahmad.ghelem@gmail.com>
2020-01-22 15:00:13 +0100

    put function for starting game

--- script.js
+++ script.js
@@ -21,3 +21,19 @@ const winningMessageTextElement = document.querySelector(
   "[data-winning-message-text]"
 );
 let circleTurn;
+
+startGame();
+
+restartButton.addEventListener("click", startGame);
+
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningMessageElement.classList.remove("show");
+}
