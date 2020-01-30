25576de7e57aa704661a48430ae8f5b4ab839c2e
AhmadGDev <Ahmad.ghelem@gmail.com>
2020-01-30 20:05:27 +0100

    add endGame function if draw or winning game

--- script.js
+++ script.js
@@ -51,3 +51,12 @@ function handleClick(e) {
     setBoardHoverClass();
   }
 }
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = "Draw!";
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+  }
+  winningMessageElement.classList.add("show");
+}
